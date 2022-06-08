ce3f4718cd90efa952c0bd3f8f7b68f092969c63
Ophélien Fouques <ophelien.fouques@viacesi.fr>
2022-06-08 15:32:05 +0200

    home pc
    Signed-off-by: Ophélien Fouques <ophelien.fouques@viacesi.fr>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div>
+      <div class="come">
       <h1>Le <span class="color">meilleur</span> endroit
         pour <span class="color">manger</span> se trouve
         dans votre canapé</h1>
@@ -8,6 +8,7 @@
 
     </div>
     <div class="bestProducts">
+        <div class="background"/>
       <ProductTile/>
       <ProductTile/>
       <ProductTile/>
@@ -28,21 +29,50 @@ export default defineComponent({
   },
 });
 </script>
-<style>
+<style lang="scss">
+    .home {
+        padding: 20px;
+        display: flex;
+        justify-content: space-around;
+
+        .come {
+            max-width: 50%;
+
+            h1 {
+                font-size: 80px;
+                text-align: left;
+            }
+
+            .searchRestaurant {
+                padding: 5px;
+                min-width: 300px;
+                border-radius: 10px;
+                border-color: #2c3e50;
+                float: left;
+            }
+
+        }
+
+        .bestProducts {
+            display: flex;
+            flex-wrap: wrap;
+            position: relative;
+
+            .background {
+                position: absolute;
+                background-color: rgba(77, 140, 160, .5);
+                right: 5%;
+                height: 95%;
+                width: 70%;
+                border-radius: 20px;
+            }
+        }
+    }
+
+
   .color {
     color: #4D90A0;
   }
 
-  .searchRestaurant {
-    padding: 5px;
-    min-width: 300px;
-    border-radius: 10px;
-    border-color: #2c3e50;
-  }
 
-
-  .bestProducts {
-    display: flex;
-    flex-wrap: wrap;
-  }
 </style>
