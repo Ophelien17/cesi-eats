0cf08ad460ca557f962f1381d68c0ef9a7f132d3
Ophélien Fouques <ophelien.fouques@viacesi.fr>
2022-06-13 10:07:54 +0200

    repair
    Signed-off-by: Ophélien Fouques <ophelien.fouques@viacesi.fr>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-]
-
-const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
-=== === =
 import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFound from '../views/NotFound.vue'
@@ -44,9 +20,8 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-        history: createWebHistory(process.env.BASE_URL),
-        routes
-    >>> >>> > nav
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
