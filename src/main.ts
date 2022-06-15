79f9895934c37db48606f7e98502f1f3241eeb2d
Léo Gilles <leo.gilles123@gmail.com>
2022-06-15 13:44:16 +0200

    Added Vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-
 import { createApp  } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import { VueCookieNext } from 'vue-cookie-next'
+import vuetify from './plugins/vuetify'
 
-const app = createApp(App).use(store).use(router)
+const app = createApp(App).use(store).use(router).use(vuetify)
 
 VueCookieNext.config({ expire: '7d' })
 app.use(VueCookieNext)
--- src/views/loginUser.vue
+++ src/views/loginUser.vue
@@ -1,40 +1,49 @@
 .<template>
-    <div class="Form">
-        <form id="formUser" ref="form" @submit="checkForm" action="http://localhost:5000/api/Users" method="post">
-            <p v-if="errors.length">
-                <b>Please correct the following error(s):</b>
-                <ul>
-                    <li v-for="error in errors" :key="error">{{ error }}</li>
-                </ul>
-            </p>
-            <p>
-                <label for="UsersName">Name :</label>
-                <input id="UsersName" v-model="UsersName" type="text" name="UsersName">
-            </p>
-            <p>
-                <label for="UsersMail">Mail :</label>
-                <input id="UsersMail" v-model="UsersMail" type="mail" name="UsersMail">
-            </p>
+    <div>
+        <v-card class="body">
+            <v-responsive :aspect-ratio="16/9">
+                <v-form ref="form" v-model="valid" lazy-validation>
+                    <v-text-field v-model="UsersName" :counter="10" :rules="nameRules" label="Name" required>
+                    </v-text-field>
 
-            <p>
-                <label for="UsersPassword">Password :</label>
-                <input id="UsersPassword" v-model="UsersPassword" type="password" name="UsersPassword">
-            </p>
+                    <v-text-field v-model="UsersMail" :rules="emailRules" label="E-mail" required></v-text-field>
 
-            <p>
-                <label for="Roles">Roles :</label>
-                <select id="Roles" v-model="Roles" name="Roles">
-                    <option value=1>Client</option>
-                    <option value=2>Livreur</option>
-                    <option value=3>Restaurateur</option>
-                </select>
-            </p>
-            <input type="hidden" name="DateOfJoining" :value="DateOfJoining" />
-            <input type="hidden" name="PhotoFileName" :value="PhotoFileName" />
-            <p>
-                <input type="submit" value="Submit">
-            </p>
-        </form>
+                    <v-text-field v-model="UsersPassword" type="password" label="Password" required></v-text-field>
+
+                    <v-select v-model="Roles" :items="items" item-value="id" item-text="name"
+                        :rules="[v => !!v || 'Role is required']" label="Roles" required>
+                    </v-select>
+
+                    <v-btn :disabled="!valid" color="success" class="mr-4" @click="SubmitCreate">
+                        Validate
+                    </v-btn>
+
+                    <v-btn color="error" class="mr-4" @click="reset1">
+                        Cancel
+                    </v-btn>
+
+                </v-form>
+
+                <v-divider class="mx-4" vertical></v-divider>
+
+                <v-form ref="form2" v-model="valid" lazy-validation>
+
+                    <v-text-field v-model="UsersMail" :rules="emailRules" label="E-mail" required></v-text-field>
+
+                    <v-text-field v-model="UsersPassword" type="password" label="Password" required></v-text-field>
+
+                    <v-btn :disabled="!valid" color="success" class="mr-4" @click="LoginUser">
+                        Validate
+                    </v-btn>
+
+                    <v-btn color="error" class="mr-4" @click="reset2">
+                        Cancel
+                    </v-btn>
+
+                </v-form>
+
+            </v-responsive>
+        </v-card>
     </div>
 </template>
 
@@ -42,92 +51,79 @@
     import axios from 'axios';
     export default {
         name: 'LoginUser',
-        data() {
-            return {
-                errors: [],
-                name: null,
-                age: null,
-                movie: null,
-                DateOfJoining: new Date(Date.now()),
-                PhotoFileName: "blank.png"
-            }
-        },
+        data: () => ({
+            valid: true,
+            UsersName: '',
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            ],
+            UsersMail: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            UsersPassword: '',
+            DateOfJoining: '',
+            PhotoFileName: '',
+            Roles: null,
+            items: [
+                "Client", "Livreur", "Restaurateur"
+            ],
+        }),
         methods: {
-
-            checkForm: function (e) {
-
-                e.preventDefault();
-
-                this.errors = [];
-
-                if (!this.UsersName) {
-                    this.errors.push('Name required.');
+            reset1() {
+                this.$refs.form.reset()
+            },
+            reset2() {
+                this.$refs.form2.reset()
+            },
+            SubmitCreate() {
+                var today = new Date();
+                var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                this.DateOfJoining = date;
+                this.PhotoFileName = "blank.png";
+                var number = 1;
+                if (this.Roles == 2) {
+                    number = 2;
                 }
-                if (!this.UsersMail) {
-                    this.errors.push('Mail required.');
+                if (this.Roles == 3) {
+                    number = 3;
                 }
-                if (!this.UsersPassword) {
-                    this.errors.push('Password required.');
-                }
-                if (!this.Roles) {
-                    this.errors.push('Roles required.');
-                }
-                if (this.UsersName && this.UsersPassword && this.UsersMail && this.Roles) {
 
-                    var today = new Date();
-                    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-                    this.DateOfJoining = date;
-                    this.PhotoFileName = "blank.png";
+                const data = JSON.stringify({
+                    UsersName: this.UsersName,
+                    UsersPassword: this.UsersPassword,
+                    UsersMail: this.UsersMail,
+                    Roles: number,
+                    DateOfJoining: this.DateOfJoining,
+                    PhotoFileName: this.PhotoFileName
+                })
 
-                    const data = JSON.stringify({
-                        UsersName: this.UsersName,
-                        UsersPassword: this.UsersPassword,
-                        UsersMail: this.UsersMail,
-                        Roles: this.Roles,
-                        DateOfJoining: this.DateOfJoining,
-                        PhotoFileName: this.PhotoFileName
+                var config = {
+                    method: 'post',
+                    url: 'http://localhost:5000/api/Users',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    data: data
+                };
+                axios(config)
+                    .then(function (response) {
+                        alert(JSON.stringify(response.data));
                     })
-
-                    var config = {
-                        method: 'post',
-                        url: 'http://localhost:5000/api/Users',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        data: data
-                    };
-                    axios(config)
-                        .then(function (response) {
-                            console.log(JSON.stringify(response.data));
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-
-
-                }
+                    .catch(function (error) {
+                        console.log(error);
+                    });
             }
+
         }
     }
 </script>
 
 <style lang="scss">
-    .Form {
+    .body {
         margin: 20px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
-        p {
-            input {
-                margin: 20px;
-                width: 200px;
-            }
-
-            select {
-                margin: 20px;
-                width: 200px;
-            }
-        }
+        padding: 20px;
     }
 </style>
